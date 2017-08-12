2f14eb4f68dbe7cbb86089dc65bf8a546262ad6e
guardian-of-kalidore <unicornsofkalidore@gmail.com>
2017-08-12 18:40:22 -0400

    Updating to load all the kore as cards into the index

--- js/kore.js
+++ js/kore.js
@@ -1,7 +1,7 @@
 <!-- retrieve owner names to auto populate input -->
 var allOwners = {};
 var allKore = {};
-function chomp(){
+function loadAllKore(){
     $.get({
         url: "https://kalidore-microservice.herokuapp.com/kore/all",
             headers: {
@@ -10,7 +10,31 @@ function chomp(){
             },
             'dataType': 'json'
         
-    }).success(function (data) {
-    	console.log(data);
+    }).success(function (koreList) {
+    	console.log(koreList);
+    	allKore = koreList;
+    	var picsDiv = $("#kore-pics");
+    	
+    	for(int i = 0; i < koreList.length; i++){
+    		var kore = koreList[i];
+    		console.log(kore);
+    		
+    		var koreDiv = $("<div>");
+    		koreDiv.attr("class", "card");
+    		
+    		koreDiv.append($("<img>").attr({
+    		"src" : "kore.mainPic",
+    		"alt" : "Avatar",
+    		"style" : "width:100%"
+    		});
+    		
+    		koreDiv.append($("<div>").attr("class", "container")
+    								.append($("<h4>").innerHtml(kore.name)));
+    		
+    		picsDiv.append(koreDiv);
+    		
+    	}
+    	
+    	
 	});
 }
